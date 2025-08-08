0cc704d6f3a11980b6e8be5c8da26a4dbc64a496
hardikdevaliya2006] <hardikdevaliya2006@gmail.com>
2025-08-08 20:53:16 +0530

    feat: Implement fetchTrendingMovieAndTV action and corresponding reducer for trending movies and TV shows

---
+++ src/Store/Actions/fetchTrendingMovieAndTV.action.js
@@ -0,0 +1,20 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import tmdbApi from "../../Services/tmdbApi";
+
+export const fetchTrendingMovieAndTV = createAsyncThunk(
+  "trendingMovieAndTVSlice/fetchTrendingMovieAndTV",
+  async (type, thunkAPI) => {
+    try {
+      const validType = type === "tv" ? "tv" : "movie";
+
+      const trendigData = await tmdbApi.get(`/trending/${validType}/week`);
+      const results = trendigData.data.results;
+
+      if (!results || results.length === 0) return null;
+
+      return { type, results };
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
---
+++ src/Store/Reducer/trendingMovieAndTV.reducer.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTrendingMovieAndTV } from "../Actions/fetchTrendingMovieAndTV.action";
+
+const trendingMovieAndTV = createSlice({
+  name: "trendingMovieAndTVSlice",
+  initialState: {
+    tvTrending: [],
+    movieTrending: [],
+    trendigDataLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTrendingMovieAndTV.pending, (state) => {
+        state.trendigDataLoading = true;
+      })
+      .addCase(fetchTrendingMovieAndTV.fulfilled, (state, action) => {
+        state.trendigDataLoading = false;
+        const { type} = action.payload;
+        if (type === "movie") {
+          state.movieTrending = action.payload;
+        } else if (type === "tv") {
+          state.tvTrending = action.payload;
+        }
+      })
+      .addCase(fetchTrendingMovieAndTV.rejected, (state, action) => {
+        state.trendigDataLoading = false;
+        state.error =
+          action.payload || "Something went wrong in trendingMovieAndTVSlice";
+      });
+  },
+});
+
+export default trendingMovieAndTV.reducer;
